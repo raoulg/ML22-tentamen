24e19520975cc2040a02aef4d96909a20349a082
Raoul Grouls <raoul.grouls@businessdecision.nl>
2023-01-16 10:02:18 +0100

    scripts

---
+++ dev/scripts/01_model_design.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+import torch
+from loguru import logger
+
+from tentamen.data import datasets
+from tentamen.model import Accuracy
+from tentamen.settings import presets
+from tentamen.train import trainloop
+
+if __name__ == "__main__":
+    logger.add(presets.logdir / "01.log")
+
+    trainstreamer, teststreamer = datasets.get_arabic(presets)
+
+    from tentamen.model import Linear
+    from tentamen.settings import LinearConfig
+
+    config = LinearConfig(
+        input=13, output=20, tunedir=presets.logdir, h1=100, h2=10, dropout=0.5
+    )
+
+    model = Linear(config.dict())  # type: ignore
+
+    trainedmodel = trainloop(
+        epochs=50,
+        model=model,  # type: ignore
+        optimizer=torch.optim.Adam,
+        learning_rate=1e-3,
+        loss_fn=torch.nn.CrossEntropyLoss(),
+        metrics=[Accuracy()],
+        train_dataloader=trainstreamer.stream(),
+        test_dataloader=teststreamer.stream(),
+        log_dir=presets.logdir,
+        train_steps=len(trainstreamer),
+        eval_steps=len(teststreamer),
+    )
+
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M")
+    path = presets.modeldir / (timestamp + presets.modelname)
+    logger.info(f"save model to {path}")
+    torch.save(trainedmodel, path)
---
+++ dev/scripts/02_tune.py
@@ -0,0 +1,75 @@
+from typing import Dict
+
+import ray
+import torch
+from filelock import FileLock
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers.hb_bohb import HyperBandForBOHB
+from ray.tune.search.bohb import TuneBOHB
+
+from tentamen.data import datasets
+from tentamen.model import Accuracy, Linear
+from tentamen.settings import SearchSpace, presets
+from tentamen.train import trainloop
+
+
+def train(config: Dict) -> None:
+    datadir = presets.datadir
+
+    with FileLock(datadir / ".lock"):
+        trainstreamer, teststreamer = datasets.get_arabic(presets)
+
+    model = Linear(config)  # type: ignore
+
+    trainloop(
+        epochs=30,
+        model=model,  # type: ignore
+        optimizer=torch.optim.Adam,
+        learning_rate=1e-3,
+        loss_fn=torch.nn.CrossEntropyLoss(),
+        metrics=[Accuracy()],
+        train_dataloader=trainstreamer.stream(),
+        test_dataloader=teststreamer.stream(),
+        log_dir=presets.logdir,
+        train_steps=len(trainstreamer),
+        eval_steps=len(teststreamer),
+        tunewriter=True,
+    )
+
+
+if __name__ == "__main__":
+    ray.init()
+
+    config = SearchSpace(
+        input=13,
+        output=20,
+        tunedir=presets.logdir,
+    )
+
+    reporter = CLIReporter()
+    reporter.add_metric_column("Accuracy")
+
+    bohb_hyperband = HyperBandForBOHB(
+        time_attr="training_iteration",
+        max_t=30,
+        reduction_factor=3,
+        stop_last_trials=False,
+    )
+
+    bohb_search = TuneBOHB()
+
+    analysis = tune.run(
+        train,
+        config=config.dict(),
+        metric="test_loss",
+        mode="min",
+        progress_reporter=reporter,
+        local_dir=config.tunedir,
+        num_samples=20,
+        search_alg=bohb_search,
+        scheduler=bohb_hyperband,
+        verbose=1,
+    )
+
+    ray.shutdown()
