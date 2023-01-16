3427f033f9186ae719cdf083ce62830c585f17fd
Raoul Grouls <raoul.grouls@businessdecision.nl>
2023-01-16 11:51:27 +0100

    results.py

---
+++ dev/scripts/03_result.py
@@ -0,0 +1,27 @@
+import torch
+from loguru import logger
+
+from tentamen.data import datasets
+from tentamen.model import Accuracy
+from tentamen.settings import presets
+from tentamen.train import evalbatches
+
+if __name__ == "__main__":
+    logger.add(presets.logdir / "01.log")
+
+    trainstreamer, teststreamer = datasets.get_arabic(presets)
+
+    timestamp = "20230116-1057"
+    path = presets.modeldir / (timestamp + presets.modelname)
+    logger.info(f"loading model from {path}")
+    model = torch.load(path)
+
+    result = evalbatches(
+        model=model,
+        testdatastreamer=teststreamer.stream(),
+        loss_fn=torch.nn.CrossEntropyLoss(),
+        metrics=[Accuracy()],
+        eval_steps=len(teststreamer),
+    )
+
+    logger.info(result)
